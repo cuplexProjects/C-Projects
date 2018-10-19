4baaa7061f4333b89ff076ac3fa531b519a204f2
Cuplex <keymaker@cuplex.se>
2018-10-19 09:02:44 +0200

    New Cuplex CDN

---
+++ SmartCuplexCdn/SmartCuplexCdn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartCuplexCdn.MvcApplication" Language="C#" %>
---
+++ SmartCuplexCdn/SmartCuplexCdn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartCuplexCdn.Startup))]
+namespace SmartCuplexCdn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
